22c9edbd3ba23b56641041b00fb8798c15c329f4
H Brown <hebrown12@gmail.com>
2023-04-28 13:29:33 -0500

    removed first-post.js

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-// import Link from 'next/link'
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
-
-
-// const name = 'Satch'
 
 export const siteTitle = 'Satch'
 
